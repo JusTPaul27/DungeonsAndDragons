e96d6602524922cf767e9b381a89eecfb7ec3d88
giovannicambiaso <giovanni.cambiaso.private@gmail.com>
2022-06-05 21:20:18 +0200

    redirect done

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,3 +1,17 @@
+const classesTemplate =
+`
+    <span class="container-title">#TITLE</span>
+    <img class="container-img" src="#IMG" alt="">
+    <span class="description-section">
+        <span class="section-title">Description:</span> 
+        #DESCRIPTION
+    </span>
+    <span class="role-section">
+        <span class="section-title">Role:</span>
+        #ROLE
+    </span>  
+`
+
 function takeClassesName() {
     const url = window.location.href;
     const startingPoint = url.indexOf('=') + 1;
@@ -5,11 +19,29 @@ function takeClassesName() {
     return name;
 }
 
+function printClasses(name, description, role) {
+    const container = document.getElementById('container');
+    container.innerHTML = '';
+  
+    const containerContent = document.createElement('div');
+    containerContent.classList.add('card');
+    const htmlTemplate = classesTemplate.replaceAll('#TITLE', name)
+                                .replaceAll('#IMG', '../assets/classes/' + name + '.png')
+                                .replaceAll('#DESCRIPTION', description)
+                                .replaceAll('#ROLE', role);
+    containerContent.innerHTML = htmlTemplate;
+    container.appendChild(containerContent);
+}
+
 function initClasses(classesJSON) {
     const classesName = takeClassesName();
-    console.log(classesJSON);
-    console.log(classesName);
-    console.log(classesJSON.classes.classesName);
+    for (const classes in classesJSON.classes) {
+        if (Object.hasOwnProperty.call(classesJSON.classes, classes)) {
+            const element = classesJSON.classes[classes];
+            if (element.name === classesName)
+                printClasses(element.name, element.description, element.role);
+        }
+    }
 }
 
 function init() {
